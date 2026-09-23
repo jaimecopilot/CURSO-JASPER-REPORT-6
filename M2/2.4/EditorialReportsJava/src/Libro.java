178cb48b68c12ea7878e4f92115af40dbda427d0
jaimecopilot <jaimegithub@outlook.com>
2026-09-23 21:48:09 +0000

    Alinea datos editoriales de 2.4 con la práctica

--- M2/2.4/EditorialReportsJava/src/Libro.java
+++ M2/2.4/EditorialReportsJava/src/Libro.java
@@ -38,16 +38,16 @@ public class Libro {
         libros.add(new Libro("Cien años de soledad", 19.95, 471, 1967, Boolean.TRUE));
         libros.add(new Libro("Rayuela", 22.50, 736, 1963, Boolean.TRUE));
         libros.add(new Libro("La ciudad y los perros", 18.75, 432, 1963, Boolean.TRUE));
-        libros.add(new Libro("Pedro Páramo", 15.90, 136, 1955, Boolean.TRUE));
+        libros.add(new Libro("Pedro Páramo", 15.90, 132, 1955, Boolean.TRUE));
         libros.add(new Libro("Ficciones", 21.00, 224, 1944, Boolean.TRUE));
         libros.add(new Libro("La casa de los espíritus", 23.40, 448, 1982, Boolean.TRUE));
         libros.add(new Libro("El amor en los tiempos del cólera", 20.80, 496, 1985, Boolean.TRUE));
         libros.add(new Libro("La muerte de Artemio Cruz", 17.60, 320, 1962, Boolean.TRUE));
         libros.add(new Libro("Doña Bárbara", 16.95, 400, 1929, Boolean.FALSE));
-        libros.add(new Libro("Martín Fierro", 14.50, 240, 1872, Boolean.FALSE));
-        libros.add(new Libro("Comala", 19.20, 288, 2024, Boolean.TRUE));
-        libros.add(new Libro("Paradiso", 25.00, 640, 1966, Boolean.TRUE));
-        libros.add(new Libro("La invención de Morel", 18.30, 160, 1940, Boolean.TRUE));
+        libros.add(new Libro("Martín Fierro", 14.50, 288, 1872, Boolean.FALSE));
+        libros.add(new Libro("Comala", 19.20, 148, 1955, Boolean.TRUE));
+        libros.add(new Libro("Paradiso", 25.00, 576, 1966, Boolean.TRUE));
+        libros.add(new Libro("La invención de Morel", 18.30, 128, 1940, Boolean.TRUE));
         libros.add(new Libro("El túnel", 16.20, 160, 1948, Boolean.FALSE));
         return libros;
     }
